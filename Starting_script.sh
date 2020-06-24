71a0b7b4ca1bdc60af16d9c379126d8d8af5fa2c
Simon Nyssen <snyssen@neuromedia.io>
2020-06-24 21:55:57 +0200

    Fix Terraria launch script

--- Starting_script.sh
+++ Starting_script.sh
@@ -3,4 +3,4 @@
 # Lancé à chaque redémarrage du serveur, il créée un nouveau screen et y lance McMyAdmin (qui a son tour lance le serveur minecraft)
 screen -S Minecraft -dm bash -c "/home/snyssen/Scripts/Launch_McMyAdmin_script.sh"
 screen -S GitBucket -dm java -jar /home/snyssen/gitbucket.war --port=8090
-screen -S Terraria -dm bash /home/snyssen/Terraria/TerrariaServer.bin.x86_64
+screen -S Terraria -dm bash -c "/home/snyssen/Scripts/Launch_Terraria.sh"
