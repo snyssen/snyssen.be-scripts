161ca3243f9358f71be71d1cd320ab54db07f5ab
Simon Nyssen <snyssen@neuromedia.io>
2020-06-25 19:05:49 +0200

    Update Terraria world

--- Launch_Terraria.sh
+++ Launch_Terraria.sh
@@ -1,3 +1,3 @@
 /home/snyssen/Terraria/TerrariaServer.bin.x86_64 \
 -port 7777 -maxplayers 16 \
--world /home/snyssen/.local/share/Terraria/Worlds/The_Fragile_Geyser.wld
+-world /home/snyssen/.local/share/Terraria/Worlds/The_Nation_of_the_Nude.wld
